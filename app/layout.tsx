79e39944ba403ac943659d327c9cf766edd54109
danchan22 <daniel.chancellor@gmail.com>
2026-08-13 13:25:39 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: '🎃 Horror Nights Tracker',
+  title: 'Horror Nights Tracker 😱',
   description: 'The scariest dashboard on earth.',
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎃</text></svg>',
