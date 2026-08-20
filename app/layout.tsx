a0fef01201d4f9cb2638b18162c114ce8cdd4b53
danchan22 <daniel.chancellor@gmail.com>
2026-08-20 14:39:57 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'HHN Tracker 😱',
-  description: 'Halloween Horror Nights Orlando',
+  title: 'HHN Tracker',
+  description: 'Halloween Horror Nights Orlando Tracker',
 };
 
 export default function RootLayout({
@@ -12,16 +12,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <style>{`
-          body {
-            margin: 0;
-            padding: 0;
-            background: #09090D url('/hhn-bg.jpg') no-repeat center top / cover !important;
-            background-attachment: scroll !important;
-          }
-        `}</style>
-      </head>
       <body>{children}</body>
     </html>
   );
