cddb941ba9ec5fcd6ea210b8c032b270e7d0d622
danchan22 <daniel.chancellor@gmail.com>
2026-08-26 13:13:20 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
-  title: 'HHN Tracker',
-  description: 'Never Go Alone 😱',
-  manifest: '/manifest.json',
+  title: 'Never Go Alone 😱',
+  description: 'Halloween Horror Nights Orlando Tracker',
+  manifest: '/manifest.webmanifest',
   appleWebApp: {
     capable: true,
     statusBarStyle: 'black-translucent',
@@ -18,3 +18,20 @@ export const viewport: Viewport = {
   maximumScale: 1,
   userScalable: false,
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+      </head>
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#12121A' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
